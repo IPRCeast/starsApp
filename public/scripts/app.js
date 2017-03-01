9793f9b0a392232bddd12674061ec1c4d6305653
evariste <evaristenk@gmail.com>
2017-03-01 10:24:32 -0800

    some templates created

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,51 @@
 
-//Define the 'starsApp' Module
+//THIS IS OUR MAIN APP FILE 'app.js' 
 
-var starsApp = angular.module('starsApp',['ngRoute']);
+//DEFINE THE MODULE AND NAME IT 'starsApp' 
 
-//configure the routes
+var starsApp = angular.module('starsApp',['ngRoute'])//configure the routes
 
-starsApp.config(['$routeProvider',function($routeProvider){
-	$routeProvider
-	//home
-	.when('/',{
-		templateUrl: "pages/home.html",
+                      .config(function ($routeProvider){
+                          $routeProvider
 
+	//ROUTE FOR THE HOME PAGE
+
+	.when('/home',{
+		templateUrl : '/views/pages/home.html',
+		controller : 'mainCtrl'
 	})
-   .when('/about',{
-		templateUrl: "pages/about.html",})
 
-   .when('/services',{
-		templateUrl: "pages/services.html",
+	////ROUTE FOR THE ABOUT PAGE
+
+   .when('/about',{
+		templateUrl : '/views/pages/about.html',
+		controller : 'aboutCtrl'
+	})
+
+   .when('/contact',{
+		templateUrl : '/views/pages/contact.html',
+		controller : 'contactCtrl'
 	});
 
-}]);
+});
+
+
+//CREATE THE CONTROLLER NAME IT 'mainCtrl' AND INJECT ANGULA'S $scope
+
+starsApp.controller('mainCtrl', function($scope){
+
+	// CREATE A MESSAGE TO DISPLAY IN OUR VIEW
+   
+    $scope.message="Hello Mr.Evariste!";
+});
+
+
+// CREATE THE CONTROLLERS AND INJECT ANGULAR'S $scope
+
+starsApp.controller('aboutCtrl', function($scope){
+         $scope.message='welcome to the About page.';
+});
+
+starsApp.controller('contacttCtrl', function($scope){
+         $scope.message='welcome to the Contact page.';
+});
