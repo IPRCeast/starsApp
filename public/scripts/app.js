b23d649fbaeee2940204f87b7e818687390183ad
evariste <evaristenk@gmail.com>
2017-03-01 02:48:32 -0800

    ngRoute library added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,4 +1,22 @@
+
 //Define the 'starsApp' Module
 
-var starsApp = angular.module('starsApp',[]);
+var starsApp = angular.module('starsApp',['ngRoute']);
 
+//configure the routes
+
+starsApp.config(['$routeProvider',function($routeProvider){
+	$routeProvider
+	//home
+	.when('/',{
+		templateUrl: "pages/home.html",
+
+	})
+   .when('/about',{
+		templateUrl: "pages/about.html",})
+
+   .when('/services',{
+		templateUrl: "pages/services.html",
+	});
+
+}]);
